f1ff5cb4a2dc3332391e46f4f21862ebbff7ace8
Atha <athaanfl@student.telkomuniversity.ac.id>
2026-09-06 22:07:54 +0700

    revert: landing page back to Home

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<ComingSoon />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:slug" element={<ProductDetail />} />
